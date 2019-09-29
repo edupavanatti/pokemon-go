fb99b55b1638217bb0200a190eb86db3022cc671
Eduardo Pavanatti <edupavanatti@gmail.com>
2019-09-29 14:06:35 -0300

    Add project

---
+++ Pokemon GO/Assets/Scripts/arremessarBola.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arremessarBola : MonoBehaviour
+{
+    bool arremessando = false;
+    float distancia = 0f;
+    public float velocidade = 10f;
+    public float velocidadeArremesso = 4f;
+    public float velocidadeBola = 4f;
+
+    void OnMouseDown()
+    {
+        distancia = Vector3.Distance(transform.position, Camera.main.transform.position);
+        arremessando = true;
+    }
+
+    public void OnMouseUp()
+    {
+        this.GetComponent<Rigidbody>().useGravity = true;
+        this.GetComponent<Rigidbody>().velocity += this.transform.forward * velocidadeArremesso;
+        this.GetComponent<Rigidbody>().velocity += this.transform.up * velocidadeBola;
+        arremessando = false;
+    }
+
+    void Update()
+    {
+        if(arremessando)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distancia);
+            transform.position = Vector3.Lerp(this.transform.position, rayPoint, velocidade * Time.deltaTime);
+        }
+    }
+}
---
+++ Pokemon GO/Assets/Scripts/checarBola.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class checarBola : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Pokemon")
+        {
+            StartCoroutine("capturarPokemon", other.gameObject);
+        }
+    }
+
+    IEnumerator capturarPokemon(GameObject Pokemon)
+    {
+        transform.Translate(Vector3.up * 1, Space.World);
+        this.GetComponent<Rigidbody>().useGravity = false;
+        this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        Destroy(Pokemon.gameObject);
+        yield return new WaitForSeconds(1);
+        this.GetComponent<Rigidbody>().useGravity = true;
+        yield return new WaitForSeconds(1);
+        GameObject.FindGameObjectWithTag("Player").transform.LookAt(this.transform);
+        GameObject.FindGameObjectWithTag("Player").gameObject.GetComponent<Camera>().fieldOfView = 10f;
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
